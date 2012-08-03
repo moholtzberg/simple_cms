4088dd67fade1d3f7deecd650cc0f749aef3e505
Moshe Holtzberg <moholtzberg@gmail.com>
2012-08-03 14:01:44 -0400

    Added default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+  
+  AdminUser.create(:username => 'Admin', :email => 'test@test.test', :hashed_password => 'ca4a6371f9d8236344288b650c1f29e9b313f52a', :salt => 'a999deb7e227b9c0188a47746008e9331869c573')
